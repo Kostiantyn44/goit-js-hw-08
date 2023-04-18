ed0ee810ae5d4688197e6dbb2e7df9e5570686cf
Kostiantyn <doroshkostiantyn@gmail.com>
2023-04-18 20:12:17 +0100

    task2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,19 @@
+import throttle from 'lodash.throttle';
+import Player from '@vimeo/player';
 
+const player = new Player('vimeo-player');
+const STORAGE_KEY = 'videoplayer-current-time';
+
+player.on(
+  'timeupdate',
+  throttle(function (data) {
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(data.seconds));
+  }, 1000)
+);
+
+const storedTime = JSON.parse(localStorage.getItem(STORAGE_KEY));
+if (storedTime) {
+  player.setCurrentTime(storedTime).catch(error => {
+    console.log('Error setting time:', error);
+  });
+}
