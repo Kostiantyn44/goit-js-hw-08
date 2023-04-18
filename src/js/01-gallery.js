444e45f0619c101782e01df16a83fb77e63c7326
Kostiantyn <doroshkostiantyn@gmail.com>
2023-04-18 19:24:42 +0100

    task1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,50 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import { galleryItems } from './gallery-items.js';
+
+function createGalleryCard(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      return `
+        <li class="gallery__item simple-lightbox">
+          <a class="gallery__link" href="${original}">
+            <img
+              class="gallery__image"
+              src="${preview}"
+              data-source="${original}"
+              alt="${description}"
+            />
+          </a>
+        </li>`;
+    })
+    .join('');
+}
+
+const cardsContainer = document.querySelector('ul.gallery');
+const cardsMarkup = createGalleryCard(galleryItems);
+cardsContainer.insertAdjacentHTML('beforeend', cardsMarkup);
+
+cardsContainer.addEventListener('click', onGalleryItemClick);
+
+function onGalleryItemClick(event) {
+  event.preventDefault();
+
+  const isGalleryItem =
+    event.target.classList.contains('gallery__image') ||
+    event.target.parentNode.classList.contains('gallery__image');
+  if (!isGalleryItem) {
+    return;
+  }
+
+  const gallery = new SimpleLightbox('.gallery a', {
+    elementsSelector: '.gallery__item:not(.simple-lightbox)',
+    docClose: false,
+    history: false,
+    fadeSpeed: 100,
+    disableScroll: false,
+    captionsData: 'alt',
+    captionDelay: 250,
+  });
+}
 
 console.log(galleryItems);
